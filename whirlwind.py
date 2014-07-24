6208903c3e7c274576b5218f5dbed2e6cd7c64b8
Ian Unruh <ianunruh@gmail.com>
2014-07-24 14:57:14 -0400

    Refactored to use python-jujuclient

--- whirlwind.py
+++ whirlwind.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
 import logging
+import os
 import random
-from subprocess import check_call, check_output
 import sys
 import time
 
+from jujuclient import Environment
 import yaml
+
+log = logging.getLogger('whirlwind')
 
 class Worker(object):
     def __init__(self, config):
         self.config = self.prepare_config(config)
-        self.logger = logging.getLogger(__name__)
+        self.env = None
 
     def start(self):
+        self.env = self.connect_state_server()
+        if not self.env:
+            return False
+
         while True:
             try:
                 if self.perform_change():
@@ -23,8 +30,19 @@ class Worker(object):
             except KeyboardInterrupt:
                 return
             except:
-                self.logger.exception('Error occured during proactive change')
+                log.exception('Error occured during proactive change')
                 return
+
+    def connect_state_server(self):
+        while True:
+            try:
+                log.debug('Connecting to state server for environment: %s', self.config.environment)
+                return Environment.connect(self.config.environment)
+            except:
+                log.exception('Could not connect to state server')
+
+            log.debug('Retrying connection in 30 seconds')
+            time.sleep(30)
 
     def perform_change(self):
         services = self.config.services.keys()
@@ -33,65 +51,63 @@ class Worker(object):
         for service in services:
             if self.try_change_service(service):
                 return True
-            
-            self.logger.debug('Service not ready for change: %s', service)
 
-        self.logger.warn('No services ready for proactive change')
+            log.debug('Service not ready for change: %s', service)
+
+        log.warn('No services ready for proactive change')
 
     def try_change_service(self, service):
         started_units = self.fetch_units(service)
         if not started_units:
             return
 
-        self.logger.debug('Performing proactive change for service: %s', service)
+        log.debug('Performing proactive change for service: %s', service)
 
-        # TODO Could use juju set-constraints --service <service> <constraints>
-
-        self.logger.debug('Adding unit to service')
-        check_call(['juju', 'add-unit', service])
+        log.debug('Adding unit to service')
+        self.env.add_unit(service)
 
         while True:
             if len(self.fetch_units(service)) > len(started_units):
                 break
 
-            self.logger.debug('Waiting 15 seconds for unit to be added')
-            time.sleep(15)
+            log.debug('Waiting 30 seconds for unit to be added')
+            time.sleep(30)
 
         unit_to_remove = random.choice(started_units.keys())
 
-        self.logger.debug('Removing unit: %s', unit_to_remove)
-        check_call(['juju', 'remove-unit', unit_to_remove])
+        log.debug('Removing unit: %s', unit_to_remove)
+        self.env.remove_units([unit_to_remove])
 
         while True:
             if unit_to_remove not in self.fetch_units(service, started=False):
                 break
 
-            self.logger.debug('Waiting 15 seconds for unit to be removed')
+            log.debug('Waiting 15 seconds for unit to be removed')
             time.sleep(15)
 
-        machine = started_units[unit_to_remove]['machine']
-
         if self.config.remove_machines:
-            self.logger.debug('Removing machine: %s', machine)
-            check_call(['juju', 'remove-machine', machine])
+            machine = started_units[unit_to_remove]['Machine']
 
-        self.logger.debug('Proactive change successful')
+            log.debug('Removing machine: %s', machine)
+            self.env.destroy_machines([machine])
+
+        log.debug('Proactive change successful')
         return True
 
     def fetch_units(self, service, started=True):
-        status = yaml.load(check_output(['juju', 'status', service]))
+        status = self.env.status()
 
-        if service not in status['services']:
+        if service not in status['Services']:
             return
 
         if not started:
-            return status['services'][service]['units'] 
+            return status['Services'][service]['Units']
 
-        return dict((k, v) for k, v in status['services'][service]['units'].iteritems() if v['agent-state'] == 'started')
+        return dict((k, v) for k, v in status['Services'][service]['Units'].iteritems() if v['AgentState'] == 'started')
 
     def prepare_config(self, config):
         config = normalize_structure(config)
-        
+
         for k, v in config.services.items():
             config.services[k] = normalize_structure(v)
 
@@ -109,7 +125,7 @@ def normalize_structure(val, recursive=False):
         return s
     elif recursive and isinstance(val, list):
         return [normalize_structure(v, True) for v in val]
-    
+
     return val
 
 class Structure(object):
@@ -124,25 +140,29 @@ def main():
 
     configure_logger(args.verbose)
 
+    if not os.path.isfile(args.config_file):
+        log.error('Could not open config file: %s', args.config_file)
+        sys.exit(1)
+
     with open(args.config_file, 'r') as fp:
         config = yaml.load(fp)
 
     worker = Worker(config)
-    worker.start()
+    if not worker.start():
+        sys.exit(1)
 
 def configure_logger(verbose):
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-    
+
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(formatter)
-    
-    root = logging.getLogger()
-    root.addHandler(handler)
-    
+
+    log.addHandler(handler)
+
     if verbose:
-        root.setLevel(logging.DEBUG)
+        log.setLevel(logging.DEBUG)
     else:
-        root.setLevel(logging.WARN)
+        log.setLevel(logging.WARN)
 
 if __name__ == '__main__':
     main()
